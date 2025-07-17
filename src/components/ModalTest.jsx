4bd63f50f05beb8f0df5aa314323b80bea0ec863
vikramkumarsfe <dk677057@gmail.com>
2025-07-17 08:30:06 +0530

    updated the modal

--- src/components/ModalTest.jsx
+++ src/components/ModalTest.jsx
@@ -1,15 +1,16 @@
 import Modal from "./reUsableComponents/Modal"
 import Card from "./reUsableComponents/Card"
 import Button from "./reUsableComponents/Button"
+import { useState } from "react"
 
 const ModalTest = ( ) => {
+    const [open,setOpen] = useState(false)
     return (
         <div className=" bg-gray-200  min-h-screen">
             <div className="w-7/12 mx-auto px-12 py-12 bg-white rounded shadow-lg  space-y-4 ">
                 <h1 className=" text-3xl font-bold text-center "> Vikram's practice </h1>
                 <p className="text-gray-500 text-base "> This is an app that demonstrating how can i make an reusable modal for showing messages in react js app.</p>
                 <hr className="w-full"/>
-                <Button color="warning"> Login</Button>
                 <div>
                     <Card
                         title="Vikram Kumar"
@@ -19,16 +20,20 @@ const ModalTest = ( ) => {
                                 <p>
                                     I am an software engineer who knows security.
                                 </p>
-                                <Button onClick={()=> alert("ABC ")}>
+                                <Button onClick={()=> setOpen(true)}>
                                     See Profie! 
                                 </Button>
                             </div>
                         }
                     />
 
-                    {/* <Modal
-                               
-                    /> */}
+                    <Modal
+                        title="vikram kumar"
+                        description = "lorem ipsum This is an app that demonstrating how can i make an reusable modal for sho "
+                        image= "./image.jpg"
+                        open={open}
+                        onClose={()=>setOpen(false)}
+                    />
                 </div>
             </div>
         </div>
--- src/components/reUsableComponents/Modal.jsx
+++ src/components/reUsableComponents/Modal.jsx
@@ -1,15 +1,17 @@
 
 
-const Modal = ({title="my name", description="My Bio", image=null }) => {
+const Modal = ({title="my name", description="My Bio", image=null, open, onClose }) => {
+    if(open)
     return (
         <div className=" bg-black bg-opacity-90  fixed top-0 left-0 min-h-screen  w-full  flex justify-center items-center ">
             <div className="bg-white rounded p-8 w-[500px] relative space-y-4 ">
                 <button
                     className="absolute top-3 right-4 "
+                    onClick={onClose}
                 >
                     <i className="ri-close-fill text-lg "></i>
                 </button>
-                <img src={image} className="h-[150px] w-[150px] rounded-full mx-auto "/>
+                { image && <img src={image} className="h-[150px] w-[150px] rounded-full mx-auto "/>}
                 <h1 className="text-3xl font-bold text-center mt-3 "> {title} </h1>
                 <p className="text-gray-600 text-justify  ">
                     {description}
